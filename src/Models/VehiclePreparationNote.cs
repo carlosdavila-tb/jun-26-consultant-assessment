369e2f5ff91a45fe6b29676a9558b647aa3f35b4
Cristian Verduga <cverduga@grupo-novatech.com>
2026-06-30 16:04:04 -0500

    feature: add  VehiclePrepationNote model

---
+++ src/Models/VehiclePreparationNote.cs
@@ -0,0 +1,20 @@
+namespace RentalDesk.Models;
+
+public class VehiclePreparationNote
+{
+    public required int Id { get; init; }
+    public required string VehicleId { get; init; }
+    public required string VehicleClass { get; init; }
+    public required DateOnly PreparationDate { get; init; }
+    public required bool ReadyForRent { get; init; }
+    public required string PreparationNote { get; init; }
+}
+
+public class CreateVehiclePreparationNote
+{
+    public string VehicleId { get; set; } = string.Empty;
+    public string VehicleClass { get; set; } = string.Empty;
+    public DateOnly PreparationDate { get; set; }
+    public bool ReadyForRent { get; set; }
+    public string PreparationNote { get; set; }
+}
